2b57a9bbef6f377290d2339997b3d14fbca670d9
avdim <avdim@mail.ru>
2021-11-21 20:09:51 +0300

    prepare bezie

--- lib/src/commonMain/kotlin/lib/vector/DisplayMode.kt
+++ lib/src/commonMain/kotlin/lib/vector/DisplayMode.kt
@@ -36,9 +36,27 @@ fun DisplayMode(modifier:Modifier, lambda: GeneratedScope.() -> Unit) {
             path = Path().apply {
               val start = points[0]
               moveTo(start.x, start.y)
-              (points.windowed(2)).forEach { (a, b) ->
-                cubicTo(a.x.f, a.y.f, b.x.f, b.y.f, b.x.f, b.y.f)
-//                lineTo(b.x, b.y)
+              (points.take(1) + points + points.takeLast(1)).windowed(4).forEach { (before, a, b, after) ->
+                val ra:Pt = a + (a - before) * 0.4f
+                val rb:Pt = b - (after - b) * 0.3f
+                // line before -> b move to center point a and scale 0.5f
+                // 1 (x.x - before.x) / (b.x - before.x) = (x.y - before.y) / (b.y - before.y)
+                // 2 (x.x - a.x) * (b.x - before.x) + (x.y - a.y) * (b.y - before.y) = 0
+                // 2 x.x = a.x -(x.y - a.y) * C1  // C1 = (b.y - before.y) / (b.x - before.x)
+                // 2 x.x = a.x -x.y*C1 + a.y * C1
+                // 1 (a.x -x.y*C1 + a.y * C1 - a.x) * (b.x - before.x) + (x.y - a.y) * C2 = 0 //C2  =(b.y - before.y)
+                // 1 (a.x -x.y*C1 + a.y * C1 - a.x) * (b.x - before.x) + x.y * C2 - a.y * C2 = 0
+                // 1 (C3 - x.y*C1) * (b.x - before.x) + x.y * C2 - a.y * C2 = 0// C3 = a.x + a.y * C1 - a.x
+                // 1 (x.y*C1 - C3) * (before.x - b.x) + x.y * C2 - a.y * C2 = 0
+                // 1 x.y * (C2 +  C1 * (before.x - b.x)) = C3 * (before.x - b.x) + a.y * C2
+                // 1 x.y = C3 * (before.x - b.x) + a.y * C2 / (C2 +  C1 * (before.x - b.x))
+                // 2 x.x = a.x -x.y*C1 + a.y * C1
+                //прямая ra, a, rb
+                val C1 = (b.y - before.y) / (b.x - before.x)
+                drawCircle(Color.Red, 2f, ra.offset)
+                drawCircle(Color.Green, 2f, rb.offset)
+//                cubicTo(ra.x.f, ra.y.f, rb.x.f, rb.y.f, b.x.f, b.y.f)
+                lineTo(b.x, b.y)
               }
             },
             Color(color),

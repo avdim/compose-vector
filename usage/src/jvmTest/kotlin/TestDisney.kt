ddacb2f95de1ef3160c64ac016590ffcc98460c7
avdim <avdim@mail.ru>
2021-12-15 12:41:03 +0300

    disney logo

--- usage/src/jvmTest/kotlin/TestDisney.kt
+++ usage/src/jvmTest/kotlin/TestDisney.kt
@@ -1,9 +1,6 @@
-package com.example.disneypluslogoanimation
-
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -13,13 +10,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.usage.runSimpleComposableWindow
 

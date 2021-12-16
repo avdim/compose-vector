617c095e53d97b8a8dd7c51cc612dd52a84851a8
avdim <avdim@mail.ru>
2021-12-17 01:15:28 +0300

    before tail2

--- usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
+++ usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
@@ -14,6 +14,9 @@ fun UsageInCommon() {
   val head1 = listOf(Pt(205, 211), Pt(196, 243), Pt(169, 259), Pt(147, 292), Pt(155, 311), Pt(173, 324), Pt(205, 338), Pt(489, 329), Pt(385, 336), Pt(304, 305), Pt(257, 256), Pt(239, 214), Pt(225, 237), Pt(167, 285), Pt(229, 227), Pt(247, 233),)
   val head2 = listOf(Pt(177, 256),Pt(175, 282),Pt(151, 297),Pt(129, 330),Pt(137, 349),Pt(155, 362),Pt(187, 376),Pt(454, 322),Pt(355, 352),Pt(284, 333),Pt(241, 292),Pt(206, 251),Pt(203, 275),Pt(149, 323),Pt(207, 265),Pt(230, 271))
 
+  val tail1 = listOf(Pt(605, 265), Pt(581, 208), Pt(596, 166), Pt(560, 179), Pt(559, 243), Pt(580, 297), Pt(553, 325), Pt(607, 190))
+  val tail2 = listOf(Pt(605, 265), Pt(581, 208), Pt(596, 166), Pt(560, 179), Pt(559, 243), Pt(580, 297), Pt(553, 325), Pt(607, 190))
+
   val infiniteTransition = rememberInfiniteTransition()
   val legsAnimationRadio by infiniteTransition.animateFloat(
     initialValue = 0f,
@@ -40,9 +43,27 @@ fun UsageInCommon() {
     )
   )
 
+  val tailAnimationRadio by infiniteTransition.animateFloat(
+    initialValue = 0f,
+    targetValue = 1f,
+    animationSpec = infiniteRepeatable(
+      animation = keyframes {
+        durationMillis = 1500
+        0.3f at 300
+        0.7f at 700
+      },
+      repeatMode = RepeatMode.Reverse
+    )
+  )
+
   val animatedHead:List<Pt> by derivedStateOf {
     val f = headAnimationRadio
     head1.mapIndexed { i, pt -> pt + (head2[i] - pt) * f }
+  }
+
+  val animatedTail:List<Pt> by derivedStateOf {
+    val f = tailAnimationRadio
+    tail1.mapIndexed { i, pt -> pt + (tail2[i] - pt) * f }
   }
 
   val animatedPointsA: List<Pt> by derivedStateOf {
@@ -53,7 +74,7 @@ fun UsageInCommon() {
     val f = legsAnimationRadio
     points2.mapIndexed { i, pt -> pt + (points1[i] - pt) * f }
   }
-//  CatBitmap()
+  CatBitmap()
 
   GeneratedLayer(Modifier) {
 
@@ -74,10 +95,19 @@ fun UsageInCommon() {
     val h42 = animatedHead[14]
     val h43 = animatedHead[15]
 
+    val t25 by mkPt(animatedTail[0])
+    val t26 by mkPt(animatedTail[1])
+    val t27 by mkPt(animatedTail[2])
+    val t28 by mkPt(animatedTail[3])
+    val t29 by mkPt(animatedTail[4])
+    val t30 by mkPt(animatedTail[5])
+    val t31 by mkPt(animatedTail[6])
+    val t44 by mkPt(animatedTail[7])
+
     val a = animatedPointsA
-    drawCurve(0xffbb009900000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15],a[16],a[17],a[18],a[19],a[20],a[21],a[22],a[23],a[24],a[25],a[26],a[27],a[28],a[29],a[30],a[31],h32,h33,h34,h35,h36,h37,h00,), mapOf(a[10] to BR(a[38], a[39]),a[11] to BR(a[40], null),h02 to BR(h41, null),h36 to BR(h42, h43),a[27] to BR(null, a[44]),a[15] to BR(a[45], null),),)
+    drawCurve(0xffbb009900000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15],a[16],a[17],a[18],a[19],a[20],a[21],a[22],a[23],a[24],t25,t26,t27,t28,t29,t30,t31,h32,h33,h34,h35,h36,h37,h00,), mapOf(a[10] to BR(a[38], a[39]),a[11] to BR(a[40], null),h02 to BR(h41, null),h36 to BR(h42, h43),t27 to BR(null, t44),a[15] to BR(a[45], null),),)
     val b = animatedPointsB
-    drawCurve(0xffbb00cc00000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,b[7],b[8],b[9],b[10],b[11],b[12],b[13],b[14],b[15],b[16],b[17],b[18],b[19],b[20],b[21],b[22],b[23],b[24],b[25],b[26],b[27],b[28],b[29],b[30],b[31],h32,h33,h34,h35,h36,h37,h00,), mapOf(b[10] to BR(b[38], b[39]),b[11] to BR(b[40], null),h02 to BR(h41, null),h36 to BR(h42, h43),b[27] to BR(null, b[44]),b[15] to BR(b[45], null),),)
+    drawCurve(0xffbb00cc00000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,b[7],b[8],b[9],b[10],b[11],b[12],b[13],b[14],b[15],b[16],b[17],b[18],b[19],b[20],b[21],b[22],b[23],b[24],t25,t26,t27,t28,t29,t30,t31,h32,h33,h34,h35,h36,h37,h00,), mapOf(b[10] to BR(b[38], b[39]),b[11] to BR(b[40], null),h02 to BR(h41, null),h36 to BR(h42, h43),t27 to BR(null, t44),b[15] to BR(b[45], null),),)
   }
 
 }

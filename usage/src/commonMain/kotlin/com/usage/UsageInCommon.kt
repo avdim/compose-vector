ed07c4c4f01a2c052cdb4292f1310c7727dc1158
avdim <avdim@mail.ru>
2021-12-16 23:26:22 +0300

    val m00 by mkPt

--- usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
+++ usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
@@ -36,26 +36,26 @@ fun UsageInCommon() {
 //  CatBitmap()
   GeneratedLayer(Modifier) {
 
-    val m00 by mkPt(205, 211)
-    val m01 by mkPt(201, 242)
-    val m02 by mkPt(169, 259)
-    val m03 by mkPt(147, 292)
-    val m04 by mkPt(155, 311)
-    val m05 by mkPt(173, 324)
-    val m06 by mkPt(205, 338)
-    val m32 by mkPt(489, 329)
-    val m33 by mkPt(385, 336)
-    val m34 by mkPt(304, 305)
-    val m35 by mkPt(257, 256)
-    val m43 by mkPt(247, 233)
-    val m36 by mkPt(239, 214)
-    val m42 by mkPt(232, 228)
-    val m37 by mkPt(225, 237)
+    val h00 by mkPt(205, 211)
+    val h01 by mkPt(201, 242)
+    val h02 by mkPt(169, 259)
+    val h03 by mkPt(147, 292)
+    val h04 by mkPt(155, 311)
+    val h05 by mkPt(173, 324)
+    val h06 by mkPt(205, 338)
+    val h32 by mkPt(489, 329)
+    val h33 by mkPt(385, 336)
+    val h34 by mkPt(304, 305)
+    val h35 by mkPt(257, 256)
+    val h43 by mkPt(247, 233)
+    val h36 by mkPt(239, 214)
+    val h42 by mkPt(232, 228)
+    val h37 by mkPt(225, 237)
 
     val a = animatedPointsA
-    drawCurve(0xff00009900000000uL,listOf(m00,m01,m02,m03,m04,m05,m06,a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15],a[16],a[17],a[18],a[19],a[20],a[21],a[22],a[23],a[24],a[25],a[26],a[27],a[28],a[29],a[30],a[31],m32,m33,m34,m35,m36,m37,m00,), mapOf(a[10] to BR(a[38], a[39]),a[11] to BR(a[40], null),a[2] to BR(a[41], null),m36 to BR(m42, m43),a[27] to BR(null, a[44]),a[15] to BR(a[45], null),),)
+    drawCurve(0xff00009900000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15],a[16],a[17],a[18],a[19],a[20],a[21],a[22],a[23],a[24],a[25],a[26],a[27],a[28],a[29],a[30],a[31],h32,h33,h34,h35,h36,h37,h00,), mapOf(a[10] to BR(a[38], a[39]),a[11] to BR(a[40], null),a[2] to BR(a[41], null),h36 to BR(h42, h43),a[27] to BR(null, a[44]),a[15] to BR(a[45], null),),)
     val b = animatedPointsB
-    drawCurve(0xFF0000cc00000000uL,listOf(m00,m01,m02,m03,m04,m05,m06,b[7],b[8],b[9],b[10],b[11],b[12],b[13],b[14],b[15],b[16],b[17],b[18],b[19],b[20],b[21],b[22],b[23],b[24],b[25],b[26],b[27],b[28],b[29],b[30],b[31],m32,m33,m34,m35,m36,m37,m00,), mapOf(b[10] to BR(b[38], b[39]),b[11] to BR(b[40], null),b[2] to BR(b[41], null),m36 to BR(m42, m43),b[27] to BR(null, b[44]),b[15] to BR(b[45], null),),)
+    drawCurve(0xFF0000cc00000000uL,listOf(h00,h01,h02,h03,h04,h05,h06,b[7],b[8],b[9],b[10],b[11],b[12],b[13],b[14],b[15],b[16],b[17],b[18],b[19],b[20],b[21],b[22],b[23],b[24],b[25],b[26],b[27],b[28],b[29],b[30],b[31],h32,h33,h34,h35,h36,h37,h00,), mapOf(b[10] to BR(b[38], b[39]),b[11] to BR(b[40], null),b[2] to BR(b[41], null),h36 to BR(h42, h43),b[27] to BR(null, b[44]),b[15] to BR(b[45], null),),)
   }
 
 }

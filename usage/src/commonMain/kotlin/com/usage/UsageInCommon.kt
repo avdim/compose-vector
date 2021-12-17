fdf0e06dde227df19d79afe9020abc5fd2fe7725
avdim <avdim@mail.ru>
2021-12-17 18:16:48 +0300

    inifinite animation

--- usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
+++ usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
@@ -2,7 +2,6 @@
 
 package com.usage
 
-import androidx.compose.animation.core.*
 import lib.vector.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -22,7 +21,7 @@ fun UsageInCommon(modifier: Modifier = Modifier) {
   Cat(modifier)
 
   if (DRAW_SNOW_DRIFT) {
-    ManySnowDrifts()
+    SnowDrifts()
   }
 
   GeneratedLayer(modifier) {
@@ -73,7 +72,7 @@ fun BackgroundHills() {
 
 
 @Composable
-fun ManySnowDrifts() {
+fun SnowDrifts() {
   val leftX = -3000f
   val rightX = 2000f
   val bottomY = 800f
@@ -83,36 +82,48 @@ fun ManySnowDrifts() {
   val stepWidth = (rightX - leftX) / pointsCount
   val speed = 3f
 
-  var snowDiffX by remember { mutableStateOf(0f) }
+  data class SnowDriftData(
+    val dx:Float,
+    val pathPoints:List<Pt>,
+    val color:ULong
+  )
+
+  var snowDrifts: List<SnowDriftData> by remember {
+    mutableStateOf(
+      List(pointsCount) {
+        val x = leftX + it * stepWidth
+        val pathPoints = listOf(
+          listOf(Pt(11, 753),Pt(135, 630),Pt(358, 578),Pt(535, 623),Pt(663, 747),),
+          listOf(Pt(37, 749),Pt(110, 663),Pt(225, 617),Pt(378, 660),Pt(445, 750),),
+          listOf(Pt(97, 756),Pt(204, 623),Pt(359, 572),Pt(523, 630),Pt(647, 747),)
+        ).random()
+
+        val baseGray = 180
+        val diffGray = (0..50).random()
+        val c = (baseGray + diffGray).toULong()
+        val color: ULong = 0xff00000000000000uL + (c shl 32) + (c shl 40) + (c shl 48)
+        SnowDriftData(x, pathPoints, color)
+      }
+    )
+  }
   LaunchedEffect(Unit) {
     while (true) {
       withFrameNanos { it }
-      snowDiffX += speed
+      snowDrifts = snowDrifts
+        .map { it.copy(dx = it.dx + speed) }
+        .toMutableList().apply {
+          indices.reversed().forEach { i->
+            if(this[i].dx > rightX) {
+              val moveMe = removeAt(i)
+              add(0, moveMe.copy(dx = leftX))
+            }
+          }
+        }
     }
   }
-  repeat(pointsCount) {
-    SnowDrift(dx = snowDiffX + remember { Random.nextInt(-3000, 2000).toFloat() })
-  }
-}
-
-@Composable
-fun SnowDrift(dx:Float, dy:Float = 0f) {
-  val pathPoints = remember {
-    listOf(
-      listOf(Pt(11, 753),Pt(135, 630),Pt(358, 578),Pt(535, 623),Pt(663, 747),),
-      listOf(Pt(37, 749),Pt(110, 663),Pt(225, 617),Pt(378, 660),Pt(445, 750),),
-      listOf(Pt(97, 756),Pt(204, 623),Pt(359, 572),Pt(523, 630),Pt(647, 747),)
-    ).random()
-  }
-  val color: ULong = remember {
-    val baseGray = 180
-    val diffGray = (0..50).random()
-    val c = (baseGray + diffGray).toULong()
-
-    0xff00000000000000uL + (c shl 32) + (c shl 40) + (c shl 48)
-  }
-  val diffPoints = pathPoints.map { it.copy(x = it.x + dx, y = it.y + dy + SNOW_DRIFT_DY) }
   DisplayMode() {
-    drawCurve(color, diffPoints, fillPath = true)
+    snowDrifts.forEach { data->
+      drawCurve(data.color, data.pathPoints.map { Pt(it.x + data.dx, it.y + SNOW_DRIFT_DY) }, fillPath = true)
+    }
   }
 }

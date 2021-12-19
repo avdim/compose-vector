7a79115fa84b3f54e4d636dc89de449909aeb8b6
avdim <avdim@mail.ru>
2021-12-19 21:59:18 +0300

    SnowDrifts

--- usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
+++ usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
@@ -9,7 +9,7 @@ import lib.vector.Pt
 import kotlin.random.Random
 
 const val DRAW_SNOW_DRIFT = true
-const val SNOW_DRIFT_DY = -30f
+const val SNOW_DRIFT_DY = -25f
 
 @OptIn(ExperimentalStdlibApi::class)
 @Composable
@@ -75,9 +75,6 @@ fun BackgroundHills() {
 fun SnowDrifts() {
   val leftX = -3000f
   val rightX = 2000f
-  val bottomY = 800f
-  val bottomLeft = Pt(leftX, bottomY)
-  val bottomRight = Pt(rightX, bottomY)
   val pointsCount = 20
   val stepWidth = (rightX - leftX) / pointsCount
   val speed = 3f
@@ -93,9 +90,11 @@ fun SnowDrifts() {
       List(pointsCount) {
         val x = leftX + it * stepWidth
         val pathPoints = listOf(
-          listOf(Pt(11, 753),Pt(135, 630),Pt(358, 578),Pt(535, 623),Pt(663, 747),),
-          listOf(Pt(37, 749),Pt(110, 663),Pt(225, 617),Pt(378, 660),Pt(445, 750),),
-          listOf(Pt(97, 756),Pt(204, 623),Pt(359, 572),Pt(523, 630),Pt(647, 747),)
+          listOf(Pt(11, 755),Pt(135, 630),Pt(358, 578),Pt(535, 623),Pt(663, 755),),
+          listOf(Pt(37, 755),Pt(110, 663),Pt(225, 617),Pt(378, 660),Pt(445, 755),),
+          listOf(Pt(97, 755),Pt(204, 623),Pt(359, 572),Pt(523, 630),Pt(647, 755),),
+          listOf(Pt(10, 755),Pt(192, 606),Pt(352, 598),Pt(458, 663),Pt(507, 717),Pt(557, 755),),
+          listOf(Pt(65, 755),Pt(166, 641),Pt(258, 629),Pt(342, 575),Pt(431, 635),Pt(495, 755),),
         ).random()
 
         val baseGray = 180
@@ -103,7 +102,7 @@ fun SnowDrifts() {
         val c = (baseGray + diffGray).toULong()
         val color: ULong = 0xff00000000000000uL + (c shl 32) + (c shl 40) + (c shl 48)
         SnowDriftData(x, pathPoints, color)
-      }
+      }.shuffled()
     )
   }
   LaunchedEffect(Unit) {
@@ -115,7 +114,7 @@ fun SnowDrifts() {
           indices.reversed().forEach { i->
             if(this[i].dx > rightX) {
               val moveMe = removeAt(i)
-              add(0, moveMe.copy(dx = leftX))
+              add(Random.nextInt(0,4), moveMe.copy(dx = leftX))
             }
           }
         }

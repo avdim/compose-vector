ed55bdf1fe99beaa49f1baa1198a23f4c0040b37
avdim <avdim@mail.ru>
2021-12-21 22:07:19 +0300

    usage

--- usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
+++ usage/src/commonMain/kotlin/com/usage/UsageInCommon.kt
@@ -4,11 +4,27 @@ package com.usage
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import lib.vector.BR
 import lib.vector.GeneratedLayer
+import lib.vector.Pt
 
 
 @OptIn(ExperimentalStdlibApi::class)
 @Composable
 fun UsageInCommon(modifier: Modifier = Modifier) {
-  GeneratedLayer(modifier) {}
+  GeneratedLayer(modifier) {
+    val m637407704 by mkPt(163, 524)
+    val m2782133651 by mkPt(67, 615)
+    val m3101024460 by mkPt(65, 619)
+    val m2429699421 by mkPt(89, 639)
+    val m2617611882 by mkPt(143, 608)
+    val m2902829938 by mkPt(203, 570)
+    val m1900164015 by mkPt(248, 534)
+
+
+    val l = listOf(Pt(171, 472),Pt(115, 465),Pt(88, 444),Pt(110, 385),Pt(113, 357),Pt(126, 363),Pt(121, 342),Pt(153, 368),Pt(230, 375),Pt(294, 371),Pt(346, 366),Pt(512, 353),Pt(605, 294),Pt(617, 213),Pt(637, 166),Pt(679, 153),Pt(661, 197),Pt(661, 252),Pt(654, 312),Pt(613, 353),Pt(581, 393),Pt(613, 438),Pt(647, 482),Pt(679, 529),Pt(700, 584),Pt(697, 619),Pt(682, 618),Pt(636, 543),Pt(564, 520),Pt(566, 560),Pt(544, 600),Pt(505, 631),Pt(460, 628),Pt(472, 608),Pt(501, 589),Pt(512, 563),Pt(463, 529),Pt(352, 511),Pt(366, 583),Pt(358, 624),Pt(323, 634),Pt(321, 605),Pt(300, 540),)
+    drawCurve(0xff0000ff00000000uL,listOf(m2782133651,m637407704,l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9],l[10],l[11],l[12],l[13],l[14],l[15],l[16],l[17],l[18],l[19],l[20],l[21],l[22],l[23],l[24],l[25],l[26],l[27],l[28],l[29],l[30],l[31],l[32],l[33],l[34],l[35],l[36],l[37],l[38],l[39],l[40],l[41],l[42],m1900164015,m2902829938,m2617611882,m2429699421,m3101024460,), mapOf(),)
+
+
+  }
 }

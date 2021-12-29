9aff8437dc9f2e736444f0579379f38cc3f37a05
avdim <avdim@mail.ru>
2021-12-29 22:44:12 +0300

    ReproduceBugScaleOffset.kt

---
+++ usage/src/jvmTest/kotlin/ReproduceBugScaleOffset.kt
@@ -0,0 +1,28 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main() = singleWindowApplication {
+  Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+      modifier = Modifier
+        .offset(-100.dp, 0.dp)
+        .scale(0.95f)
+        .offset(200.dp, 20.dp)
+        .size(100.dp)
+        .background(Color.LightGray)
+    )
+    Box(
+      modifier = Modifier
+        .offset(0.dp, 0.dp)
+        .scale(0.95f)
+        .offset(200.dp, 20.dp)
+        .size(100.dp)
+        .background(Color.LightGray)
+    )
+  }
+}

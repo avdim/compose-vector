d1e0b67fd3eb744ed2667dcf3a4c85579eaf326b
avdim <avdim@mail.ru>
2022-01-07 14:30:53 +0300

    ColorPicker

--- lib/src/jvmMain/kotlin/lib/vector/ColorPicker.kt
+++ lib/src/jvmMain/kotlin/lib/vector/ColorPicker.kt
@@ -9,41 +9,45 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.*
 
 @Composable
 fun ColorPicker(currentColor: ULong, onChangeColor: (ULong) -> Unit) {
   var dialogOpen by remember { mutableStateOf(false) }
-    Row(modifier = Modifier.clickable {
-        dialogOpen = !dialogOpen
-    }) {
-        Canvas(modifier = Modifier.size(30.dp)) {
-            drawRect(color = Color(currentColor))
-        }
-        Text("color")
+  fun select(color: ULong) {
+    dialogOpen = false
+    onChangeColor(color)
+  }
+  Row(modifier = Modifier.clickable {
+    dialogOpen = !dialogOpen
+  }) {
+    Canvas(modifier = Modifier.size(30.dp)) {
+      drawRect(color = Color(currentColor))
     }
+    Text("color")
+  }
   if (dialogOpen) {
-      Window(
-          state = WindowState(width = 400.dp, height = 800.dp, position = WindowPosition(0.dp, 0.dp)),
-          onCloseRequest = {
-              dialogOpen = false
-          }) {
-          Column {
-              TxtButton("Blue") {
-                  onChangeColor(Color.Blue.value)
-              }
-              TxtButton("Red") {
-                  onChangeColor(Color.Red.value)
-              }
-              TxtButton("Yellow") {
-                  onChangeColor(Color.Yellow.value)
-              }
-          }
+    Dialog(
+      state = DialogState(width = 400.dp, height = 400.dp, position = WindowPosition(Alignment.TopStart)),
+      onCloseRequest = {
+        dialogOpen = false
       }
+    ) {
+      Column {
+        TxtButton("Blue") {
+          select(Color.Blue.value)
+        }
+        TxtButton("Red") {
+          select(Color.Red.value)
+        }
+        TxtButton("Yellow") {
+          select(Color.Yellow.value)
+        }
+      }
+    }
   }
 }

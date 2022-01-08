b47033ae6bf8b6d103ce0da0557e088bbdcaed92
avdim <avdim@mail.ru>
2022-01-08 10:16:59 +0300

    TestScroll.kt

--- usage/src/jvmTest/kotlin/TestScroll.kt
+++ usage/src/jvmTest/kotlin/TestScroll.kt
@@ -1,17 +1,27 @@
+import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.*
 import com.usage.runSimpleComposableWindow
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() = runSimpleComposableWindow {
   Box(
-    Modifier.fillMaxSize().onPointerEvent(PointerEventType.Scroll) {
-      val scrollDelta = it.changes.first().scrollDelta
-      println("scrollDelta: $scrollDelta")
-    }
+    Modifier.fillMaxSize()
+      .pointerInput(Unit) {
+        while (true) {
+          val event = awaitPointerEventScope {
+            awaitPointerEvent()
+          }
+          if (event.type == PointerEventType.Scroll) {
+            println("scroll $event")
+            event.changes.first().scrollDelta.y
+            event.keyboardModifiers.isShiftPressed//right
+            event.keyboardModifiers.isCtrlPressed//zoom
+          }
+        }
+      }
   )
 }
